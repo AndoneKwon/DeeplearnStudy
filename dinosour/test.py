d73d8771e5c287671ed98d5fbad0cdbbc0cd78d9
AndoneKwon <45382126+AndoneKwon@users.noreply.github.com>
2019-05-20 22:55:49 +0900

    learning
    learning

--- dinosour/test.py
+++ dinosour/test.py
@@ -4,6 +4,7 @@ import numpy as np
 import keyboard
 import glob
 import csv
+#import tensorflow as tf
 from pynput.mouse import Controller
 from PIL import ImageGrab
 
@@ -110,8 +111,53 @@ def screen_record():
         
 
 def Learning():
-    file=open("C:/Users/권진우/Documents/GitHub/DeeplearnStudy/dinosour/data.csv","r",newline='')#파일을 생성 혹은 오픈
+    learning_rate = 0.001
+    training_epochs = 15
+    batch_size = 100
+    learn_variable=7
+    nb_classes=3
+    data=np.loadtxt('C:/Users/권진우/Documents/GitHub/DeeplearnStudy/dinosour/data.csv',delimiter=',',dtype=np.float32)#파일을 읽기전용으로 오픈
+    x_data=data[:,0:-1]
+    y_data=data[:,[-1]]
+    X = tf.placeholder(tf.float32, [None, learn_variable])
+    # 0 - 9 digits recognition = 10 classes
+    Y = tf.placeholder(tf.float32, [None, 3])
+    
+    W1 = tf.Variable(tf.random_normal([learn_variable, 10]))
+    b1 = tf.Variable(tf.random_normal([10]))
+    layer1=tf.nn.relu(tf.matmul(X,W1)+b1)
+    
+    W2 = tf.Variable(tf.random_normal([10, 10]))
+    b2 = tf.Variable(tf.random_normal([10]))
+    layer2=tf.nn.relu(tf.matmul(layer1,W2)+b2)
+
+
+    W3 = tf.Variable(tf.random_normal([10,nb_classes]))
+    b3 = tf.Variable(tf.random_normal([nb_classes]))
+    hypothesis = tf.nn.sigmoid(tf.matmul(layer2, W3) + b3)
+
+    # define cost/loss & optimizer
+    cost = -tf.reduce_mean(Y*tf.log(hypothesis)+(1-Y)*tf.log(1-hypothesis))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    # initialize
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+
+    # train my model
+    print('Learning started. It takes sometime.')
+    for epoch in range(training_epochs):
+        avg_cost = 0
+        total_batch = int(mnist.train.num_examples / batch_size)
+        
+        for i in range(total_batch):
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            feed_dict = {X: x_data, Y: batch_ys}
+            c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
+            avg_cost += c / total_batch
     
         
-screen_record()
+#screen_record()
 file.close()
+print(x_data.shape,x_data)
+print(y_data.shape,y_data)
